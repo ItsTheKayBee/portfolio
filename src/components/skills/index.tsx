39d1fda76cd6e612e60a3f2db90a704130a58c3e
Kunal Bohra <kbohra89@gmail.com>
2022-03-18 23:14:09 +0530

    star animations

--- src/components/skills/index.tsx
+++ src/components/skills/index.tsx
@@ -1,7 +1,8 @@
-import { faStar, faStarHalf } from '@fortawesome/free-solid-svg-icons'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import HalfStar from 'components/icon/HalfStar'
+import Star from 'components/icon/Star'
 import { Image as ImageType, SkillsType } from 'data/interface'
 import Image from 'next/image'
+import styles from './index.module.scss'
 import sectionStyles from 'styles/section.module.scss'
 
 const Skills = ({ title, data }: SkillsType): JSX.Element => {
@@ -19,7 +20,7 @@ const Skills = ({ title, data }: SkillsType): JSX.Element => {
 
 const Skill = ({ image, ratings }: { image: ImageType; ratings: number }) => {
 	return (
-		<div>
+		<div className={styles.skill}>
 			<Image src={image.url} alt={image.alt} height={50} width={50} />
 			<Stars count={ratings} />
 		</div>
@@ -28,13 +29,13 @@ const Skill = ({ image, ratings }: { image: ImageType; ratings: number }) => {
 
 const Stars = ({ count }: { count: number }): JSX.Element => {
 	return (
-		<div>
+		<div className={styles.starsContainer}>
 			{Array(Math.floor(count))
 				.fill(0)
 				.map((_, key) => (
-					<FontAwesomeIcon icon={faStar} key={key} />
+					<Star key={key} />
 				))}
-			{count % 1 !== 0 && <FontAwesomeIcon icon={faStarHalf} />}
+			{count % 1 !== 0 && <HalfStar />}
 		</div>
 	)
 }
