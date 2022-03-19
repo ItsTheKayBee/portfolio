dc55f4fec9032b64e1602e862ae060417245a012
Kunal Bohra <kbohra89@gmail.com>
2022-03-19 20:44:12 +0530

    Added blobs

--- src/components/skills/index.tsx
+++ src/components/skills/index.tsx
@@ -4,24 +4,55 @@ import { Image as ImageType, SkillsType } from 'data/interface'
 import Image from 'next/image'
 import styles from './index.module.scss'
 import sectionStyles from 'styles/section.module.scss'
+import { SVG_CLIP_PATHS } from 'helpers/constants'
 
 const Skills = ({ title, data }: SkillsType): JSX.Element => {
 	return (
 		<div className={sectionStyles.section}>
 			<h1 className={sectionStyles.sectionTitle}>{title}</h1>
-			<div>
+			<div className={styles.skills}>
 				{data.map((skill, key) => {
-					return <Skill key={key} image={skill.image} ratings={skill.ratings} />
+					return (
+						<Skill
+							key={key}
+							image={skill.image}
+							ratings={skill.ratings}
+							id={key}
+						/>
+					)
 				})}
 			</div>
 		</div>
 	)
 }
 
-const Skill = ({ image, ratings }: { image: ImageType; ratings: number }) => {
+const Skill = ({
+	image,
+	ratings,
+	id
+}: {
+	image: ImageType
+	ratings: number
+	id: number
+}) => {
 	return (
 		<div className={styles.skill}>
-			<Image src={image.url} alt={image.alt} height={50} width={50} />
+			<Image
+				src={image.url}
+				alt={image.alt}
+				height={300}
+				width={300}
+				className={styles[`skillImage${id + 1}`]}
+			/>
+			<svg viewBox='0 0 200 200' fill='none' className={styles.blob}>
+				<clipPath
+					id={`clipper${id + 1}`}
+					clipPathUnits='objectBoundingBox'
+					className={styles.clipPath}
+				>
+					<path d={SVG_CLIP_PATHS[id]} />
+				</clipPath>
+			</svg>
 			<Stars count={ratings} />
 		</div>
 	)
