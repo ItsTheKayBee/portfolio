9a5160fe7c9abe527d04d0b8898d5b5a97068b38
Kunal Bohra <kbohra89@gmail.com>
2022-03-23 20:20:21 +0530

    Bg change

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,8 @@
 import dynamic from 'next/dynamic'
-import '../styles/globals.scss'
 import { portfolioDataObject, statsData } from 'data'
+import '../styles/globals.scss'
+import { useEffect, useState } from 'react'
+import { TOTAL_NUMBER_OF_SECTIONS } from 'helpers/constants'
 
 const About = dynamic(() => import('../components/about'))
 const Achievements = dynamic(() => import('../components/achievements'))
@@ -11,15 +13,43 @@ const Publications = dynamic(() => import('../components/publications'))
 const Skills = dynamic(() => import('../components/skills'))
 
 const App = (): JSX.Element => {
+	const [currentSection, setCurrentSection] = useState(0)
+
+	useEffect(() => {
+		window.addEventListener('wheel', scroller)
+		return () => window.removeEventListener('wheel', scroller)
+	}, [])
+
+	const scroller = (e: WheelEvent) => {
+		console.log(e)
+		setCurrentSection(s => {
+			if (e.deltaY > 0 && s < TOTAL_NUMBER_OF_SECTIONS - 1) return s + 1
+			else if (e.deltaY < 0 && s > 0) return s - 1
+			else return s
+		})
+	}
+
 	return (
 		<main className='main'>
-			<About {...portfolioDataObject.about} stats={statsData.stats} />
-			<Experience {...portfolioDataObject.experiences} />
-			<Skills {...portfolioDataObject.skills} />
-			<Projects {...portfolioDataObject.projects} />
-			<Achievements {...portfolioDataObject.achievements} />
-			<Publications {...portfolioDataObject.publications} />
-			<Contact {...portfolioDataObject.contact} />
+			<div className='bg'>
+				{Array(4)
+					.fill(0)
+					.map((_, key) => (
+						<div
+							className={'aurora aurora-' + (currentSection + 1)}
+							key={key}
+						/>
+					))}
+			</div>
+			<div>
+				<About {...portfolioDataObject.about} stats={statsData.stats} />
+				<Experience {...portfolioDataObject.experiences} />
+				<Skills {...portfolioDataObject.skills} />
+				<Projects {...portfolioDataObject.projects} />
+				<Achievements {...portfolioDataObject.achievements} />
+				<Publications {...portfolioDataObject.publications} />
+				<Contact {...portfolioDataObject.contact} />
+			</div>
 		</main>
 	)
 }
