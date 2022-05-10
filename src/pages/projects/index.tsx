e3fd915f8fb33323ec446482f78d6d3b7bec581c
Kunal Bohra <kbohra89@gmail.com>
2022-05-10 19:31:47 +0530

    Dragging setup

--- src/pages/projects/index.tsx
+++ src/pages/projects/index.tsx
@@ -1,3 +1,4 @@
+import { useRef, useState } from 'react'
 import Image from 'next/image'
 import Arrow from 'components/icon/Arrow'
 import { portfolioDataObject } from 'data'
@@ -8,13 +9,58 @@ import styles from './index.module.scss'
 const Projects = (): JSX.Element => {
 	const { title, data }: ProjectsType = portfolioDataObject.projects
 
+	const containerRef = useRef<HTMLDivElement>(null)
+	const [pos, setPos] = useState({
+		top: 0,
+		left: 0,
+		x: 0,
+		y: 0
+	})
+	const [grab, setGrab] = useState(false)
+
+	const mouseDownHandler = (e: any) => {
+		setPos({
+			// The current scroll
+			left: containerRef.current?.scrollLeft ?? 0,
+			top: containerRef.current?.scrollTop ?? 0,
+			// Get the current mouse position
+			x: e.clientX,
+			y: e.clientY
+		})
+		setGrab(true)
+	}
+
+	const mouseMoveHandler = (e: any) => {
+		if (grab) {
+			const dx = e.clientX - pos.x
+			const dy = e.clientY - pos.y
+
+			// Scroll the element
+			containerRef.current!.scrollTop = pos.top - dy
+			containerRef.current!.scrollLeft = pos.left - dx
+		}
+	}
+
+	const mouseUpHandler = () => {
+		setGrab(false)
+	}
+
 	return (
-		<div className={`${sectionStyles.section} ${styles.projects}`}>
-			<img src='/images/projects.svg' className={styles.bg} />
-			<h1 className={sectionStyles.sectionTitle}>{title}</h1>
-			{data.map((project, key) => {
-				return <Project key={key} {...project} id={key} />
-			})}
+		<div
+			className={`${sectionStyles.section} ${styles.projects}`}
+			onMouseDown={mouseDownHandler}
+			onMouseMove={mouseMoveHandler}
+			onMouseUp={mouseUpHandler}
+			style={{ cursor: grab ? 'grabbing' : 'grab' }}
+			ref={containerRef}
+		>
+			<div className={styles.projectContainer}>
+				<img src='/images/projects.svg' className={styles.bg} />
+				<h1 className={sectionStyles.sectionTitle}>{title}</h1>
+				{data.map((project, key) => {
+					return <Project key={key} {...project} id={key} top={730} left={0} />
+				})}
+			</div>
 		</div>
 	)
 }
@@ -26,13 +72,16 @@ const Project = ({
 	description,
 	technologies,
 	button,
-	id = 0
-}: ProjectsData): JSX.Element => {
+	id = 0,
+	top,
+	left
+}: ProjectsData & { top: number; left: number }): JSX.Element => {
 	return (
 		<div
-			className={`${sectionStyles.subSection} ${styles.project} ${
+			className={`${styles.project} ${
 				id % 2 !== 0 ? sectionStyles.reverse : ''
 			}`}
+			style={{ top: top, left: left }}
 		>
 			<div className='col a-center'>
 				<Image
