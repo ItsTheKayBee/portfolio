a59b0de2da1270a1322936bd4db020bcebc74ef7
ItsTheKayBee <kunal.bohra@somaiya.edu>
2022-07-31 15:46:26 +0000

    Deploying to gh-pages from @ ItsTheKayBee/portfolio@6a218bfad7e246ac6d3bbb6b51c2ce0b81f30c23 🚀

---
+++ _next/static/GnWFZASYjHQmhZica0jcD/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/947-284799f7dd5fb1d7.js","static/css/a01fd578c9e56781.css","static/chunks/pages/index-b5b0e1c7e8ea538e.js"],"/_error":["static/chunks/pages/_error-0a004b8b8498208d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
